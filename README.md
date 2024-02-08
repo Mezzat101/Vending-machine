dec258dbfc90b75c0e1a1eb60465f42ec8704986
Mezzat101 <mohamsdezzat@gmail.com>
2024-02-08 04:18:24 +0200

    Update Project

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# Vending-machine
+# Vending Machine C-Program 😊 
+
+Documentation
+ This documentation provides a detailed explanation of the vending machine program implemented in C programming language.
+ ## Structure 🚧
+ The program consists of the following components:
+
+* Struct Definition: Defines a structure named node to store product information. Each node contains the name of the product (name), the quantity of the product (quantity), the cost of the product (cost), and pointers to the next and previous nodes (next and prev).
+
+* Global Variables: Declares global variables start and end to store the start and end of the linked list representing the vending machine.
+
+* Function Declarations: Declares function prototypes for all functions used in the program.
+
+* Main Function: The main() function serves as the entry point of the program. It provides a menu-driven interface for users to interact with the vending machine.
+
+* Function Definitions: Contains the implementations of all functions declared earlier in the program.
+
+## Functionality 🌝
+* Initialization
+     * *init_list()*: Initializes the linked list representing the vending machine. Sets both start and end pointers to NULL.
+
+* Node Creation
+    * *creat_node()*: Creates a new node with the provided product information (name, quantity, cost) and returns a pointer to the newly created node.
+
+* Node Insertion
+    * *insert_node()*: Inserts a new node into the linked list in ascending order based on the quantity of the product. Handles cases when the list is empty, the new node should be inserted at the beginning, or in the middle of the list.
+
+* Printing
+    * *print_list()*: Prints the contents of the linked list, i.e., the products available in the vending machine.
+
+* Searching
+    * *search_node()*: Searches for a product in the vending machine by its name. Returns a pointer to the node if the product is found, otherwise returns NULL.
+
+* Deletion
+   * *delete_node()*: Deletes a product from the vending machine based on its name. Handles cases when the product to be deleted is at the beginning, at the end, or in the middle of the list.
+
+* Destruction
+    * *destroy_list()*: Destroys the entire linked list representing the vending machine. Frees the memory allocated for each node in the list.
+
+## User Interaction 🙆
+The main() function provides a user-friendly menu-driven interface for interacting with the vending machine. Users can perform the following actions:
+
+* Insert a product into the vending machine by providing its name, quantity, and cost.
+Print the list of products available in the vending machine.
+
+* Delete a product from the vending machine by specifying its name.
+
+* Search for a product in the vending machine by specifying its name.
+
+* Destroy the entire vending machine, removing all products from the list.
+
+## Flow Control 🎛️
+The program utilizes a do-while loop to repeatedly display the menu and prompt the user for input until the user chooses to exit by pressing 'x'.
+
+## Error Handling ⁉
+* The program handles cases where the user inputs an invalid option by defaulting to printing the list of products.
+
+* Error messages are displayed if a product is not found during deletion or search operations.
+
+* Memory allocation errors are not explicitly handled in this program.
+
+# Project Demo 🧐
+<video controls src="vanding machine-1.mp4" title="Title"></video>
--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include <malloc.h>
 
 /* This is a structure to store the product information */
-struct node {
+struct node
+{
     char name[20];
-    int  quantity;
+    int quantity;
     float cost;
     struct node *next, *prev;
 };
@@ -29,55 +30,63 @@ int main()
     char i, z, name[10];
     float cost;
     init_list();
-    do {
-printf("Press 1 to insert a product\nPress 2 to print the list\nPress 3 to delete a product\nPress 4 to search for a product\nPress 5 to destroy the list\n");
-printf("===================================\n");
-scanf("%d",&x);
-    if (x==1){
-        printf("Insert product:\nname:\n");
-        scanf("%s",name);
-        printf("quantity:\n");
-        scanf("%d",&quant);
-        printf("cost:\n");
-        scanf("%f",&cost);
-        struct node *ptr;
-        ptr=creat_node(name,quant,cost);
-        insert_node(ptr);
-        printf("The product is added\n");
-    }
-    else if (x==3){
-        printf("Enter the product you want to delete:\n");
-        scanf("%s",name);
-        delete_node(name);
-    }
-    else if (x==4){
-        printf("Enter the product you want to search for:\n");
-        scanf("%s",name);
-        struct node *result;
-        result=search_node(name);
-        if (result=='\0'){
-            printf("The product is not exist\n");
+    do
+    {
+        printf("Press 1 to insert a product\nPress 2 to print the list\nPress 3 to delete a product\nPress 4 to search for a product\nPress 5 to destroy the list\n");
+        printf("===================================\n");
+        scanf("%d", &x);
+        if (x == 1)
+        {
+            printf("Insert product:\nname:\n");
+            scanf("%s", name);
+            printf("quantity:\n");
+            scanf("%d", &quant);
+            printf("cost:\n");
+            scanf("%f", &cost);
+            struct node *ptr;
+            ptr = creat_node(name, quant, cost);
+            insert_node(ptr);
+            printf("The product is added\n");
         }
-        else {
-            printf("The product:\n%s\n%d\n%f\n",result->name,result->quantity,result->cost);
+        else if (x == 3)
+        {
+            printf("Enter the product you want to delete:\n");
+            scanf("%s", name);
+            delete_node(name);
         }
-    }
-    else if (x==5){
-        destroy_list();
-    }
-    else {
-        print_list();
-    }
-    printf("please enter (y) to return or (x) to exit:\n");
-    scanf(" %c",&z);
-    }
-    while(z!='x');
+        else if (x == 4)
+        {
+            printf("Enter the product you want to search for:\n");
+            scanf("%s", name);
+            struct node *result;
+            result = search_node(name);
+            if (result == '\0')
+            {
+                printf("The product is not exist\n");
+            }
+            else
+            {
+                printf("The product:\n%s\n%d\n%f\n", result->name, result->quantity, result->cost);
+            }
+        }
+        else if (x == 5)
+        {
+            destroy_list();
+        }
+        else
+        {
+            print_list();
+        }
+        printf("please enter (y) to return or (x) to exit:\n");
+        scanf(" %c", &z);
+    } while (z != 'x');
     return 0;
 }
 
 /*********************************** Definition *******************************/
 /* This function initializes the list */
-void init_list() {
+void init_list()
+{
     start = end = '\0';
 }
 /* This function creates a new node and returns a pointer to it */
@@ -97,22 +106,31 @@ struct node *creat_node(char *ptr, int val1, float val2)
 void insert_node(struct node *element)
 {
     struct node *temp = start;
-    if (start == end && start == '\0') {
+    if (start == end && start == '\0')
+    {
         start = element;
         end = element;
-    } else {
-        while (temp != '\0' && temp->quantity < element->quantity) {
+    }
+    else
+    {
+        while (temp != '\0' && temp->quantity < element->quantity)
+        {
             temp = temp->next;
         }
-        if (temp == '\0') {
+        if (temp == '\0')
+        {
             element->prev = end;
             end->next = element;
             end = element;
-        } else if (temp == start) {
+        }
+        else if (temp == start)
+        {
             element->next = start;
             start->prev = element;
             start = element;
-        } else {
+        }
+        else
+        {
             element->next = temp;
             element->prev = temp->prev;
             temp->prev->next = element;
@@ -125,10 +143,12 @@ void insert_node(struct node *element)
 void print_list()
 {
     struct node *temp = start;
-    if (temp == '\0') {
+    if (temp == '\0')
+    {
         printf("There is no list\n");
     }
-    while (temp != '\0') {
+    while (temp != '\0')
+    {
         printf("%s\n%d\n%f\n", temp->name, temp->quantity, temp->cost);
         temp = temp->next;
     }
@@ -138,7 +158,8 @@ void print_list()
 struct node *search_node(char *ptr)
 {
     struct node *temp = start;
-    while (temp->name != ptr && temp != '\0') {
+    while (temp->name != ptr && temp != '\0')
+    {
         temp = temp->next;
     }
     return temp;
@@ -148,21 +169,30 @@ struct node *search_node(char *ptr)
 void delete_node(char *ptr)
 {
     struct node *temp = search_node(ptr);
-    if (temp == '\0') {
+    if (temp == '\0')
+    {
         printf("The object is not exist\n");
-    } else if (temp == start && start == end) {
+    }
+    else if (temp == start && start == end)
+    {
         start = '\0';
         end = '\0';
         free(temp);
-    } else if (temp == start) {
+    }
+    else if (temp == start)
+    {
         start = temp->next;
         start->prev = '\0';
         free(temp);
-    } else if (temp == end) {
+    }
+    else if (temp == end)
+    {
         end = temp->prev;
         end->next = '\0';
         free(temp);
-    } else {
+    }
+    else
+    {
         temp->next->prev = temp->prev;
         temp->prev->next = temp->next;
         free(temp);
@@ -174,7 +204,7 @@ void destroy_list()
 {
     struct node *temp;
     while (start != '\0')
-        {
+    {
         temp = start;
         start = start->next;
         free(temp);
